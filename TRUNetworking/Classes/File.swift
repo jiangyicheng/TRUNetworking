7ed1334d7929bc28c621f30f9c12bf0371014e66
姜易成 <530554582@qq.com>
2020-03-07 17:18:14 +0800

    组件2

---
+++ TRUNetworking/Classes/File.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  TRUNetworking
+//
+//  Created by 橙子 on 2020/3/7.
+//
+
+import Foundation
+
+public func addAction(){
+    print("组件化初体验")
+}
+
+public func jyc_action(){
+    print("----")
+}
